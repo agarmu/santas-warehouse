86528cf6ffb9bb4af82567b22b98744dc18a483b
Mukul Agarwal <55563106+agarmu@users.noreply.github.com>
2020-12-26 07:54:03 -0600

    feat: added api endpoints interfacing with database

--- src/pages/api/getfeedback.ts
+++ src/pages/api/getfeedback.ts
@@ -3,6 +3,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function Hi(_req: NextApiRequest, res: NextApiResponse) {
   const { db } = await connectToDatabase();
-  const movies = await db.collection(`testing123`).find({}).limit(20).toArray();
-  res.status(200).send(movies);
+  const feedback = await db.collection(`feedback`).find({}).limit(20).toArray();
+  res.status(200).send(feedback);
 }
