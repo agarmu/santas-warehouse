3e815762844f8fe8034be40e2a9efb16c78f70ee
Mukul Agarwal <55563106+agarmu@users.noreply.github.com>
2020-12-26 10:45:38 -0600

    fix: add necessary JS files for Bootstrap
    	- jquery.min.js
    	- popper.min.js
    	- bootstrap.min.js

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -28,6 +28,9 @@ export default function Layout({
       </Head>
       <Header />
       <Container>{children}</Container>
+      <script type='text/javascript' src='/js/jquery.min.js'></script>
+      <script type='text/javascript' src='/js/popper.min.js'></script>
+      <script type='text/javascript' src='/js/bootstrap.min.js'></script>
     </div>
   );
 }
